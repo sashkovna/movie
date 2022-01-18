c4c52e3b6a9c34068545ea8b100fa32184390c22
Anna <any9684@rambler.ru>
2022-01-18 18:58:16 +0300

    react-movie

---
+++ README.md
@@ -0,0 +1,3 @@
+# Проект React Movies для курса React с нуля для начинающих 
+
+Рабочую версию приложения можно посмотреть на [GitHub Pages](https://michey85.github.io/react-movies/).
---
+++ src/components/Search.jsx
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+
+const Search = (props) => {
+    const {searchMovies = Function.prototype} = props
+    const [search, setSearch] = useState('')
+    const [type, setType] = useState('all')
+
+    const handleKey = (event) => {
+        if (event.key === 'Enter') {
+            searchMovies(search, type);
+        }
+    };
+
+    const handleFilter = (event) => {
+        setType(event.target.dataset.type)
+        searchMovies(search, event.target.dataset.type)
+    };
+
+        return (
+            <div className='row'>
+                <div className='input-field'>
+                    <input
+                        className='validate'
+                        placeholder='search'
+                        type='search'
+                        value={search}
+                        onChange={(e) =>
+                            setSearch(e.target.value)
+                        }
+                        onKeyDown={handleKey}
+                    />
+                    <button
+                        className='btn search-btn'
+                        onClick={() => searchMovies(search, type)}
+                    >
+                        Search
+                    </button>
+                </div>
+                <div>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='type'
+                            type='radio'
+                            data-type='all'
+                            onChange={handleFilter}
+                            checked={type === 'all'}
+                        />
+                        <span>All</span>
+                    </label>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='type'
+                            type='radio'
+                            data-type='movie'
+                            onChange={handleFilter}
+                            checked={type === 'movie'}
+                        />
+                        <span>Movies only</span>
+                    </label>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='type'
+                            type='radio'
+                            data-type='series'
+                            onChange={handleFilter}
+                            checked={type === 'series'}
+                        />
+                        <span>Series Only</span>
+                    </label>
+                </div>
+            </div>
+        );
+}
+
+export { Search };
